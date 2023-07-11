5647123f46692afaf93580c26f2f03c67ad189f0
fuadayodeji <fuadayodeji@gmail.com>
2023-07-11 19:34:16 +0100

    Script to Change UPN in Azure AD

---
+++ ChangingUPN.ps1
@@ -0,0 +1,11 @@
+﻿#change UPN
+Connect-AzureAD
+$domain = "faradaf007.onmicrosoft.com"
+Get-AzureADUser -All $True  | Where { $_.UserPrincipalName.EndsWith("faradaf.com") }  | 
+ForEach {
+ $newupn = $_.UserPrincipalName.Split("@")[0] + "@" + $domain
+ Set-AzureADUser -ObjectId $_.objectid  -UserPrincipalName $newupn
+}
+
+
+
