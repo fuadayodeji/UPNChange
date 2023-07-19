e375cc7e3c1bf374b2c035d94a64d6cd0378cddb
fuadayodeji <44568161+fuadayodeji@users.noreply.github.com>
2023-07-19 07:31:14 +0100

    Update ChangingUPN.ps1

--- ChangingUPN.ps1
+++ ChangingUPN.ps1
@@ -1,11 +1,11 @@
-﻿#change UPN
+#change UPN
 Connect-AzureAD
 $domain = "faradaf007.onmicrosoft.com"
-Get-AzureADUser -All $True  | Where { $_.UserPrincipalName.EndsWith("faradaf.com") }  | 
-ForEach {
- $newupn = $_.UserPrincipalName.Split("@")[0] + "@" + $domain
- Set-AzureADUser -ObjectId $_.objectid  -UserPrincipalName $newupn
+
+# Get users with the old domain in UPN and update their UPNs
+$usersToUpdate = Get-AzureADUser -All $true | Where-Object { $_.UserPrincipalName.EndsWith("faradaf.com") }
+
+foreach ($user in $usersToUpdate) {
+    $newUpn = $user.UserPrincipalName.Split("@")[0] + "@" + $domain
+    Set-AzureADUser -ObjectId $user.ObjectId -UserPrincipalName $newUpn
 }
-
-
-
